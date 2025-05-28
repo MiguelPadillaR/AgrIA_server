5e0424a589b0777e290177b094f625393be96cf0
MiguelPadillaR <m.padi.r@icloud.com>
2025-05-28 09:59:35 +0200

    Reworked folder and project structure + Added initial image description prompts

---
+++ server/routes/hello.py
@@ -0,0 +1,8 @@
+# app/routes/hello.py
+from flask import Blueprint, jsonify
+
+hello_bp = Blueprint('hello', __name__)
+
+@hello_bp.route('/hello-world', methods=['GET'])
+def hello_world():
+    return jsonify("Hello world!")
