a891b010719d5844a02905f3dcf0e674912d67fe
MiguelPadillaR <m.padi.r@icloud.com>
2025-05-28 12:03:17 +0200

    Fixed LLM prompting references + Updated .gitignore

--- server/__init__.py
+++ server/__init__.py
@@ -1,13 +1,15 @@
+import shutil
 from flask import Flask
 from flask_cors import CORS
 from .config import UI_URL
 from .routes.hello import hello_bp
 from .routes.user_input import user_input_bp
 from .routes.image_upload import image_upload_bp
-
 def create_app():
     app = Flask(__name__)
     CORS(app, resources={r"/*": {"origins": UI_URL}})
+    # Delete temp files
+    shutil.rmtree('temp')
 
     # Register Blueprints
     app.register_blueprint(hello_bp)
