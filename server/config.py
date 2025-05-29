619955b2ceff7b0498ffe1977e6929fdca1bcf4a
MiguelPadillaR <m.padi.r@icloud.com>
2025-05-29 12:12:56 +0200

    Refactored prompt files + Minor fixes

--- server/config.py
+++ server/config.py
@@ -7,9 +7,13 @@ load_dotenv()
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 UI_URL = os.getenv("UI_URL", "http://localhost:4200")
 
+if not GEMINI_API_KEY:
+    raise ValueError("GEMINI_API_KEY is not set in .env")
+
+# Initialize the Google Generative AI client and chat session
 client = genai.Client(api_key=GEMINI_API_KEY)
 model_name = "gemini-2.0-flash"
 chat = client.chats.create(model=model_name)
 
-if not GEMINI_API_KEY:
-    raise ValueError("GEMINI_API_KEY is not set in .env")
+short_image_description_prompt_path = "LLM-tldr_desc_prompt.txt"
+full_image_description_prompt_path = "LLM-full_desc_prompt.txt"
