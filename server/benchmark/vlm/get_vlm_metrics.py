f99f490a751d4b07b82368ef85206ef8e9f473c3
MiguelPadillaR <m.padi.r@icloud.com>
2025-11-05 09:39:04 +0000

    Added benchmark using JSON ecoscheme classif. algorithm input with LLM versus LLM standalone + Added brief role prompting + Updated .gitignore

--- server/benchmark/vlm/get_vlm_metrics.py
+++ server/benchmark/vlm/get_vlm_metrics.py
@@ -9,18 +9,18 @@ from PIL import Image
 from google import genai
 from google.genai import types
 
-from ...services.sigpac_tools_v2.find import find_from_cadastral_registry
-from ...utils.parcel_finder_utils import reset_dir
-from ...config.constants import SEN2SR_SR_DIR, TEMP_DIR
-from ...services.sen2sr.constants import GEOJSON_FILEPATH
-
-
-from .constants import BM_JSON_DIR, BM_LLM_DIR, BM_SR_IMAGES_DIR, CADASTRAL_REF_LIST_PAPER, DATES_PAPER, IS_PAPER_DATA
-from ...services.parcel_finder_service import download_sen2sr_parcel_image
 from ..sr.utils import copy_file_to_dir
-from ...utils.chat_utils import generate_image_context_data
+from ...config.constants import SEN2SR_SR_DIR, TEMP_DIR
 from ...config.llm_client import client
+from ...services.parcel_finder_service import download_sen2sr_parcel_image
+from ...services.sen2sr.constants import GEOJSON_FILEPATH
+from ...services.sigpac_tools_v2.find import find_from_cadastral_registry
+from ...utils.chat_utils import generate_image_context_data
+from ...utils.parcel_finder_utils import reset_dir
 
+
+from .constants import BM_JSON_DIR, BM_LLM_DIR, BM_PROMPT_LIST_FILE, BM_SR_IMAGES_DIR, CADASTRAL_REF_LIST_PAPER, DATES_PAPER, FULL_DESC_SYS_INSTR_EN, FULL_DESC_SYS_INSTR_ES, IS_PAPER_DATA, OG_CLASSIFICATION_FILEPATH
+from .ecoscheme_classif_algorithm import calculate_ecoscheme_payment_exclusive
 from .llm_setup import generate_system_instructions
 from .utils import n_random_dates_between
 
@@ -49,7 +49,7 @@ def setup_ndates(n_dates:int, to_date: datetime=datetime.today(), delta_days: in
     
     return dates
 
-def get_parcel_data_and_description(cadastral_ref, image_date):
+def get_parcel_data_and_description(cadastral_ref, image_date, lang):
     # Get parcel metadata and geometry
     geometry, metadata = find_from_cadastral_registry(cadastral_ref)
     os.makedirs(SEN2SR_SR_DIR, exist_ok=True)
@@ -61,7 +61,7 @@ def get_parcel_data_and_description(cadastral_ref, image_date):
     land_uses = metadata.get('usos', None)
     query = metadata.get('query', None)
     parcel_metadata = generate_image_context_data(image_date, land_uses, query)
-    parcel_desc = parcel_metadata['en']
+    parcel_desc = parcel_metadata[lang]
     logger.debug(f"Parcel description: {parcel_desc}")
     
     # Extract parcel area safely
@@ -87,22 +87,47 @@ def get_parcel_image(cadastral_ref, geometry, image_date):
 
     return image_filepath
 
-def get_llm_response(image_filepath, parcel_desc): 
+def get_llm_full_desc(image_filepath: str, parcel_desc: str, lang: str=None, classification_filepath: str=OG_CLASSIFICATION_FILEPATH):
+    json_data = None
+    if lang:
+        with open(classification_filepath, 'r') as file:
+            all_rules_list = json.load(file)[lang.upper()]
+        rules_json_str = json.dumps(all_rules_list) 
+        json_data = calculate_ecoscheme_payment_exclusive(parcel_desc, rules_json_str)
+    
+    prompt_en = f"Produce a full English parcel description with the following information. Combine it with the image for a 700-character max description:\n\n{json_data}"
+    desc_en = "The satellite image displays a **31.18-hectare** agricultural parcel dominated by **dry, reddish-brown tones**, indicative of **low vegetation cover** or recently worked soil, typical of Mediterranean woody crops or extensive grazing lands. This visual assessment directly correlates with the digital land use analysis. The largest area, **Olive Groves (OV)**, accounts for **30.72 ha** and has been assigned to the Eco-scheme **P6/P7 (Plant Cover)**, based on the highest payment/ha. A smaller portion of **Shrub Pastures (PR)**, **0.36 ha**, is assigned to **P1 (Extensive Grazing)**.The overall low vegetation signal in the image is consistent with the extensive farming practices suggested by the two applicable Eco-schemes, particularly the large area of OV allocated to the P6/P7 scheme using the Tier 2 rate."
+    prompt_en += f'\n\nThis is an example of the 700-char max parcel descriptiom:\n"{desc_en}"'
+    prompt_es = f"Genera una descripción completa de la parcela en Español con la siguiente información. Combínala con la imagen para la descripción de 700 caracteres máximo:\n\n{json_data}"
+    desc_es = "La imagen satélite muestra una parcela agrícola de **31.18 ha**, dominada por **tonos rojizos, marrones y secos**, indicativo de **poca cobertura vegetal** o terreno trabajado recientemente, típico de cultivos leñosos mediterráneos o de tierras de pastoreo extensivo. La evaluación visual se correlaciona direcatmente con el análisi digital del uso de la tierra. El área más grande, **Olivares (OV)**, abarca **30.72 ha** y ha sido asignaco al Ecorrégimen **P6/P7 (Cobertura Vegetal)**, debido a tener el importe pago/ha más alto. Un pequeña porción de **Pasto Arbustivo (PA)**, **0.36 ha**, ha sido asignada a **P1 (Pastoreo Extensivo)**. La baja señal de vegetación en general en la imagen is consistente con las prácticas agrícolas extensivas sugeridas en estos dos Ecorregímenes aplicables, particularmente, el área más grande de OV que se ha asignado a P6/P7 usando importes del Tramo 2."
+    prompt_es += f'\n\Este es un ejemplo de la la descripción de 700 caracteres máximo:\n"{desc_es}"'
     prompt = f"Describe the parcels based on the following information and image:\n\n{parcel_desc}"
+
+    prompt = prompt_en if lang is "en" else prompt_es if lang is "es" else prompt
     logger.debug(f'Prompt generated:\n"{prompt[:150]}..."')
+
     image = Image.open(image_filepath)
     logger.debug(f"Image loaded for inference")
 
-    response = client.models.generate_content(
+    sys_ins = (
+        FULL_DESC_SYS_INSTR_EN if lang == "en"
+        else FULL_DESC_SYS_INSTR_ES if lang == "es"
+        else generate_system_instructions()
+    )
+
+    # logger.debug(f"LLM instructions: {sys_ins}")
+
+    llm_response = client.models.generate_content(
         model="gemini-2.5-flash",
         config=types.GenerateContentConfig(
-            system_instruction=generate_system_instructions()
+            system_instruction=sys_ins
             ),
         contents=[image, prompt]
     )
-    logger.debug(f"AgrIA's response:\n---BEGIN\n{response.text}\n---END")
+    logger.debug(f"AgrIA's response:\n---BEGIN\n{llm_response.text}\n---END")
 
-    return response
+    return llm_response, json_data
+
 
 def extract_json_from_reply(raw_text: str):
     # Split into lines and remove first line (e.g., ```json or BEGIN)
@@ -125,9 +150,9 @@ def extract_json_from_reply(raw_text: str):
 
     # Sanity check
     if not cleaned_text or "{" not in cleaned_text:
-        print("[WARNING] No valid JSON found in LLM response, skipping...")
+        logger.warning("No valid JSON found in LLM response, skipping...")
 
-    # Write to temp file
+    # Save ecoschemes JSON classification
     output_path = BM_LLM_DIR / f"{cadastral_ref}_out.json"
     with open(output_path, "w", encoding="utf-8") as f:
         f.write(cleaned_text)
@@ -138,22 +163,14 @@ def extract_json_from_reply(raw_text: str):
         with open(output_path, "r", encoding="utf-8") as f:
             data = json.load(f)
     except json.JSONDecodeError as e:
-        print(f"[ERROR] Failed to decode JSON: {e}")
-        print("[DEBUG] Cleaned text preview:\n", cleaned_text[:300])
+        logger.error(f"[ERROR] Failed to decode JSON: {e}")
+        logger.debug("[DEBUG] Cleaned text preview:\n", cleaned_text[:300])
     logger.debug(f"Data from JSON file:\n{data}")
 
     # --- Normalize JSON into dataframe ---
     json_df = pd.json_normalize(
         data,
-        record_path=["Estimated_Total_Payment"],
-        meta=[
-            "Report_Type",
-            "Total_Parcel_Area_ha",
-            ["Calculation_Context", "Rate_Applied"],
-            ["Calculation_Context", "Source"],
-            ["Final_Results", "Total_Aid_without_Pluriannuality_EUR"],
-            ["Final_Results", "Total_Aid_with_Pluriannuality_EUR"],
-        ],
+        sep=".",
         errors="ignore"
     )
     return json_df
@@ -167,6 +184,9 @@ try:
     # Get all cadastral references and date data
     cadastral_ref_list = CADASTRAL_REF_LIST_PAPER if IS_PAPER_DATA else ["26002A001000010000EQ", "14048A001001990000RM","45054A067000090000QA", "43157A024000010000KE", "34039A005000020000YQ", "27020A319000010000QL", "43022A037000430000JO", "50074A045000370000KA", "50074A014000730000KS", "25015A501101860000RF", "25142A002000430000BP", "22121A007001610000UD", "22145A011000110000PI", "22061A018000530000GG", "26002A004009350000EI", "41079A057000020000JR", "41012A018000030000TX", "23086A02500051FA", "23060A065002370000EX", "29055A040000040000HL", "41062A012001000000UQ", "41062A012000960000UB", ""]  # TODO
     dates = DATES_PAPER if IS_PAPER_DATA else setup_ndates(len(cadastral_ref_list))
+    
+    lang="en"
+    # lang=None
 
     for cadastral_ref in cadastral_ref_list:
         if len(cadastral_ref) is not 20:
@@ -177,7 +197,7 @@ try:
         
         # Get parcel input data
         image_date = dates[i]
-        geometry, parcel_desc = get_parcel_data_and_description(cadastral_ref, image_date)
+        geometry, parcel_desc = get_parcel_data_and_description(cadastral_ref, image_date, lang)
         image_filepath = get_parcel_image(cadastral_ref, geometry, image_date)
         
         # Add data to input df
@@ -193,21 +213,49 @@ try:
         
         reset_dir(TEMP_DIR)
         
-
         # Run LLM and get response
-        raw_text = get_llm_response(image_filepath, parcel_desc).text.strip()
+        raw_text, json_data = get_llm_full_desc(image_filepath, parcel_desc, lang)
+        # Save full desc to file
+        full_desc_filepath = BM_LLM_DIR / f"{cadastral_ref}_full_desc_{lang}.md"
+        if json_data:
+            with open(full_desc_filepath, "w") as f: 
+                f.write(raw_text.text.strip())
+        output_path = BM_LLM_DIR / f"{cadastral_ref}_out.json"
+        with open(output_path, "w", encoding="utf-8") as f:
+            json.dump(json_data, f, indent=4)
         exec_time = str(timedelta(seconds=(datetime.now() - init_time).total_seconds()))
         # Parse LLM reply
-        json_df = extract_json_from_reply(raw_text)
-        es_list = json_df["Ecoscheme_ID"].values.tolist().sort()
+        json_df = extract_json_from_reply(raw_text.text.strip()) if not json_data else json_data
+        if json_data:
+            es_list = sorted({
+                item["Ecoscheme_ID"]
+                for item in json_df["Estimated_Total_Payment"]
+                if item.get("Ecoscheme_ID") and item["Ecoscheme_ID"] != "N/A"
+            })
+            parcel_area = json_df.get('Total_Parcel_Area_ha', [None])
+            predicted_base_aid = json_df.get('Final_Results').get('Total_Aid_without_Pluriannuality_EUR', [None])
+            predicted_plur_aid = json_df.get('Final_Results').get('Total_Aid_with_Pluriannuality_EUR', [None]),
+        else:
+            es_list =sorted([
+                item["Ecoscheme_ID"]
+                for sublist in json_df["Estimated_Total_Payment"]
+                for item in sublist
+                if isinstance(item, dict) and "Ecoscheme_ID" in item
+            ])
+            parcel_area = json_df.get('Total_Parcel_Area_ha', [None])[0]
+            predicted_base_aid = json_df.get('Final_Results.Total_Aid_without_Pluriannuality_EUR', [None])[0]
+            predicted_plur_aid = float(json_df.get('Final_Results.Total_Aid_with_Pluriannuality_EUR', [None])[0]),
+        
+        predicted_plur_aid = predicted_plur_aid[0]
+
         # es_list = str(es_list).replace("'", "").replace('"', "").replace("[", "").replace("]", "") if IS_PAPER_DATA else es_list
         out_row = pd.DataFrame([{
             'cadastral_ref': cadastral_ref,
-            'parcel_area': json_df.get('Total_Parcel_Area_ha', [None])[0],
+            'parcel_area': parcel_area,
             'land_uses_amount': len(parcel_desc.split("Land")) - 1,
             'predicted_ecoschemes': es_list,
-            'predicted_base_aid': json_df.get('Final_Results.Total_Aid_without_Pluriannuality_EUR', [None])[0],
-            'predicted_plur_aid': json_df.get('Final_Results.Total_Aid_with_Pluriannuality_EUR', [None])[0],
+            'predicted_base_aid': predicted_base_aid,
+            'predicted_plur_aid': predicted_plur_aid,
             'exec_time': exec_time,
         }])
         
@@ -219,6 +267,7 @@ try:
         time_taken_formatted = str(timedelta(seconds=time_taken))
         logger.debug(f"Time taken for parcel processing {time_taken_formatted}")
         total_time += time_taken
+
 finally:
     total_time_formatted = str(timedelta(seconds=total_time))
     logger.debug(f"BENCHMARK EXEC. TIME {total_time_formatted}")
